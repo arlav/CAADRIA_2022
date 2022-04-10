5b0c9670dcefd544b958a7c2ead996bfb70c45c6
arlav <dounas@gmail.com>
2022-04-10 16:03:26 +0100

    Create Urban_Minter.sol
    adapting the nft minter for test networks

---
+++ contracts/Urban_Minter.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+//original code is from nftschool.dev
+
+
+pragma solidity ^0.7.0;
+
+// @dev change the openzeppelin paths if you are using this contract locally with hardhat
+// @dev uncomment the following line if you are using hardhat locally 
+
+// import "hardhat/console.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/utils/Counters.sol";
+
+contract UrbanMinter is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory tokenName, string memory symbol) ERC721(tokenName, symbol) {
+        _setBaseURI("ipfs://");
+    }
+
+    function mintToken(address owner, string memory metadataURI)
+    public
+    returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 id = _tokenIds.current();
+        _safeMint(owner, id);
+        _setTokenURI(id, metadataURI);
+
+        return id;
+    }
+}
